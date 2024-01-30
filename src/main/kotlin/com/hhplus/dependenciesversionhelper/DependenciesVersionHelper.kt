c8ba9e150c74bd82da81aa9fd9748b5e244b5ae0
hanbee <sonhanbi1002@gmail.com>
2024-01-30 22:12:38 +0900

    feat: 프로젝트 오픈 시 build.gradle 파일에서 springboot version 조회

---
+++ src/main/kotlin/com/hhplus/dependenciesversionhelper/DependenciesVersionHelper.kt
@@ -0,0 +1,63 @@
+package com.hhplus.dependenciesversionhelper
+
+import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.project.ProjectManager
+import com.intellij.openapi.project.ProjectManagerListener
+import com.intellij.util.messages.MessageBusConnection
+import java.util.regex.Pattern
+
+
+class DependenciesVersionHelper : ProjectManagerListener {
+    private val connection: MessageBusConnection
+
+    init {
+        connection = ApplicationManager.getApplication().messageBus.connect()
+        connection.subscribe(ProjectManager.TOPIC, this)
+    }
+
+    override fun projectOpened(project: Project) {
+        // 프로젝트가 열릴 때 실행될 로직
+        val springBootVersion = findSpringBootVersion(project)
+        println(">>>>> SpringBootVersion: $springBootVersion")
+        springBootVersion?.let { retrieveSpringBootDependencies(it) }
+    }
+
+    fun dispose() {
+        connection.disconnect()
+    }
+
+    private fun findSpringBootVersion(project: Project): String? {
+        // build.gradle 또는 pom.xml에서 스프링 부트 버전 추출 로직
+        val projectFile = project.projectFile
+        val projectBaseDir = projectFile?.parent?.parent
+
+        val buildGradleFile = projectBaseDir?.findChild("build.gradle")
+
+        if (buildGradleFile != null) {
+            try {
+                val buildGradleContent = String(buildGradleFile.contentsToByteArray())
+                println(">>>>> buildGradleContent:\n $buildGradleContent")
+                return extractSpringBootVersion(buildGradleContent)
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+        return null
+    }
+
+    private fun retrieveSpringBootDependencies(springBootVersion: String) {
+        // Maven Central에서 spring-boot-dependencies POM 파일 다운로드 및 파싱 로직
+    }
+
+    companion object {
+        private fun extractSpringBootVersion(buildGradleContent: String): String? {
+            // val pattern = Pattern.compile("org\\.springframework\\.boot:spring-boot-gradle-plugin:(\\d+\\.\\d+\\.\\d+\\.RELEASE)")
+            val pattern = Pattern.compile("id 'org.springframework.boot' version '([\\d.]+)'")
+            val matcher = pattern.matcher(buildGradleContent)
+            return if (matcher.find()) {
+                matcher.group(1)
+            } else null
+        }
+    }
+}
