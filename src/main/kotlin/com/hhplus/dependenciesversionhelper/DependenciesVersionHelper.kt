73c90cd3b59467fe1190eb05646599907cdaad55
hanbee <sonhanbi1002@gmail.com>
2024-01-31 21:05:59 +0900

    feat: spring-boot-dependencies 버전에 맞는 pom 조회 후 Dependency 클래스 목록으로 변환

--- src/main/kotlin/com/hhplus/dependenciesversionhelper/DependenciesVersionHelper.kt
+++ src/main/kotlin/com/hhplus/dependenciesversionhelper/DependenciesVersionHelper.kt
@@ -5,7 +5,12 @@ import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManager
 import com.intellij.openapi.project.ProjectManagerListener
 import com.intellij.util.messages.MessageBusConnection
+import org.w3c.dom.Element
+import org.w3c.dom.Node
+import java.io.InputStream
+import java.net.URL
 import java.util.regex.Pattern
+import javax.xml.parsers.DocumentBuilderFactory
 
 
 class DependenciesVersionHelper : ProjectManagerListener {
@@ -27,7 +32,8 @@ class DependenciesVersionHelper : ProjectManagerListener {
         // 프로젝트가 열릴 때 실행될 로직
         val springBootVersion = findSpringBootVersion(project)
         println(">>>>> SpringBootVersion: $springBootVersion")
-        springBootVersion?.let { retrieveSpringBootDependencies(it) }
+        springBootVersion?.let { downloadSpringBootDependenciesPom(it) }
+        // springBootVersion?.let { retrieveSpringBootDependencies(it) }
     }
 
     fun dispose() {
@@ -53,8 +59,39 @@ class DependenciesVersionHelper : ProjectManagerListener {
         return null
     }
 
-    private fun retrieveSpringBootDependencies(springBootVersion: String) {
-        // Maven Central에서 spring-boot-dependencies POM 파일 다운로드 및 파싱 로직
+    private fun downloadSpringBootDependenciesPom(springBootVersion: String) {
+        val pomUrl = "https://repo1.maven.org/maven2/org/springframework/boot/spring-boot-dependencies/" +
+                "$springBootVersion/spring-boot-dependencies-$springBootVersion.pom"
+
+        try {
+            URL(pomUrl).openStream().use { inputStream ->
+                val dependencies = parseDependenciesFromPom(inputStream)
+                dependencies.forEach { println(it) }
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+
+    private fun parseDependenciesFromPom(inputStream: InputStream): List<Dependency> {
+        val factory = DocumentBuilderFactory.newInstance()
+        val builder = factory.newDocumentBuilder()
+        val input = builder.parse(inputStream)
+
+        val dependencies = mutableListOf<Dependency>()
+        val nodes = input.getElementsByTagName("dependency")
+
+        for (i in 0 until nodes.length) {
+            val node = nodes.item(i)
+            if (node.nodeType == Node.ELEMENT_NODE) {
+                val element = node as Element
+                val groupId = element.getElementsByTagName("groupId").item(0).textContent
+                val artifactId = element.getElementsByTagName("artifactId").item(0).textContent
+                dependencies.add(Dependency(groupId, artifactId))
+            }
+        }
+
+        return dependencies
     }
 
     companion object {
---
+++ src/main/kotlin/com/hhplus/dependenciesversionhelper/Dependency.kt
@@ -0,0 +1,3 @@
+package com.hhplus.dependenciesversionhelper
+
+data class Dependency(val groupId: String, val artifactId: String)
